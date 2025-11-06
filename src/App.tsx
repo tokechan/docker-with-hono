cfdc508a615b38a1cb374bf7133def516e316665
tokec <fleatoke@gmail.com>
2025-11-06 15:34:12 +0900

    Enhance App component with controlled input for new todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [title, setTitle] = useState("");
   const todos = [
     {
       id: 1,
@@ -28,8 +30,14 @@ function App() {
         {todos.map((todo) => (
           <div key={todo.id}>{todo.title}</div>
         ))}
-        <input type="text" name="title" placeholder="Add a new todo" />
+        <input 
+        type="text"
+        placeholder="Add a new todo"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        />
         <button type="submit">Add</button>
+        <div>{title}</div>
       </div>
     </>
   );
