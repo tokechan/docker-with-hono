28a519ccc28cb8bc41204c21fd330b84d8995fb9
tokec <fleatoke@gmail.com>
2025-11-06 15:58:54 +0900

    Add functionality to handle adding new todos in App component

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { useState } from 'react';
 
 function App() {
   const [title, setTitle] = useState("");
-  const todos = [
+  const [todos, setTodos] = useState([
     {
       id: 1,
       title: "todo 1",
@@ -19,7 +19,19 @@ function App() {
       title: "todo 3",
       completed: false,
     },
-  ];
+  ]);
+
+  const handleAddTodo = () => {
+    setTodos([
+      ...todos,
+      {
+        id: todos.length + 1,
+        title: title,
+        completed: false,
+      },
+    ]);
+    setTitle("");
+  };
 
   console.log(todos);
 
@@ -35,8 +47,13 @@ function App() {
         placeholder="Add a new todo"
         value={title}
         onChange={(e) => setTitle(e.target.value)}
+        className='border-2 border-gray-300 rounded-md p-2'
         />
-        <button type="submit">Add</button>
+        <button 
+        type="submit" 
+        className='bg-blue-500 text-white rounded-md p-2'
+        onClick={handleAddTodo}
+        >Add</button>
         <div>{title}</div>
       </div>
     </>
